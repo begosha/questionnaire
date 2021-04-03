31811f8ca6a69b1697801bd6be7e4d3e93991cfe
Gosha <begoshaaaa@gmail.com>
2021-04-03 15:48:51 +0600

    path to delete choice is fixed

--- source/webapp/urls.py
+++ source/webapp/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import (IndexView, PollAddView, PollView, PollUpdateView, PollDeleteView, ChoiceCreateView, ChoiceUpdateView)
+from .views import (IndexView, PollAddView, PollView, PollUpdateView, PollDeleteView, ChoiceCreateView, ChoiceUpdateView, ChoiceDeleteView)
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
@@ -8,5 +8,6 @@ urlpatterns = [
     path('<int:pk>/update/poll', PollUpdateView.as_view(), name='poll-update'),
     path('<int:pk>/delete', PollDeleteView.as_view(), name='poll-delete'),
     path('<int:pk>/add/', ChoiceCreateView.as_view(), name='choice-add'),
-    path('<int:pk>/update', ChoiceUpdateView.as_view(), name='choice-update')
+    path('<int:pk>/update', ChoiceUpdateView.as_view(), name='choice-update'),
+    path('<int:pk>/delete/choice', ChoiceDeleteView.as_view(), name='choice-delete')
 ]
