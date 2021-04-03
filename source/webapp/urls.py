fa8bd82f5007ad1d9583bc3b0a0c8e6e32c8abb5
Gosha <begoshaaaa@gmail.com>
2021-04-03 14:24:12 +0600

    path to choice add is added

--- source/webapp/urls.py
+++ source/webapp/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from webapp.views.poll_views import (IndexView, PollAddView, PollView, PollUpdateView, PollDeleteView)
+from .views import (IndexView, PollAddView, PollView, PollUpdateView, PollDeleteView, ChoiceCreateView)
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
@@ -7,4 +7,5 @@ urlpatterns = [
     path('add/', PollAddView.as_view(), name='poll-add'),
     path('<int:pk>/update/poll', PollUpdateView.as_view(), name='poll-update'),
     path('<int:pk>/delete', PollDeleteView.as_view(), name='poll-delete'),
+    path('<int:pk>/add/', ChoiceCreateView.as_view(), name='choice-add'),
 ]
