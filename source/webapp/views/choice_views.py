11fa5ad0e67fe4a9a65b7771618e780cfaff703b
Gosha <begoshaaaa@gmail.com>
2021-04-03 14:24:43 +0600

    choice add view is added

---
+++ source/webapp/views/choice_views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render, get_object_or_404, redirect
+from ..models import Poll, Choice
+from ..forms import SimpleSearchForm, PollForm, ChoiceForm
+from django.urls import reverse, reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.db.models import Q
+from django.utils.http import urlencode
+from django.http import HttpResponseRedirect
+
+
+class ChoiceCreateView(CreateView):
+    # template_name = 'choice/task_add_view.html'
+    form_class = ChoiceForm
+    model = Choice
+
+    def get_success_url(self):
+        return reverse(
+            'poll',
+            kwargs={'pk': self.kwargs.get('pk')}
+        )
+
+    def form_valid(self, form):
+        poll = get_object_or_404(Poll, id=self.kwargs.get('pk'))
+        form.instance.poll = poll
+        return super().form_valid(form)
