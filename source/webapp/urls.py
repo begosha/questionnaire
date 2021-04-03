2a7d82f71e8dd44b8f2171768f54df64151006da
Gosha <begoshaaaa@gmail.com>
2021-04-03 12:33:49 +0600

    path to poll detail view is done

--- source/webapp/urls.py
+++ source/webapp/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from webapp.views.poll_views import (IndexView, PollAddView)
+from webapp.views.poll_views import (IndexView, PollAddView, PollView)
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
+    path('<int:pk>/', PollView.as_view(), name='poll'),
     path('add/', PollAddView.as_view(), name='poll-add'),
 ]
