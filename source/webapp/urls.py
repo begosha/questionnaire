dfc404baafbc0b434efb5fd66b721ca5a6d359e0
Gosha <begoshaaaa@gmail.com>
2021-04-03 12:39:51 +0600

    path to poll update view is added

--- source/webapp/urls.py
+++ source/webapp/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from webapp.views.poll_views import (IndexView, PollAddView, PollView)
+from webapp.views.poll_views import (IndexView, PollAddView, PollView, PollUpdateView)
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
     path('<int:pk>/', PollView.as_view(), name='poll'),
     path('add/', PollAddView.as_view(), name='poll-add'),
+    path('<int:pk>/update/poll', PollUpdateView.as_view(), name='poll-update'),
 ]
