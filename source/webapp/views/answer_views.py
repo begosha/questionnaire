fa58f6f864a091eb8c9d83bfd3671bd32d5fe7bf
Gosha <begoshaaaa@gmail.com>
2021-04-03 22:05:27 +0600

    saving the answer is fixed

--- source/webapp/views/answer_views.py
+++ source/webapp/views/answer_views.py
@@ -9,25 +9,31 @@ from django.views.generic import CreateView
 class AnswerAddView(CreateView):
     template_name = 'answer/result_page.html'
     form_class = AnswerForm
-    model = Answer
+
+    def get(self, request, *args, **kwargs):
+        form = self.get_form()
+        poll = Poll.objects.get(id=self.kwargs.get('pk'))
+        context = {'form': form,'poll': poll}
+        return render(request, self.template_name, context)
 
     def get_form(self, form_class=None):
         if form_class is None:
             form_class = self.get_form_class()
         poll = Poll.objects.get(id=self.kwargs.get('pk'))
-        print('hgjhgfg',poll)
         return form_class(poll, **self.get_form_kwargs())
 
     def form_valid(self, form):
-        poll = get_object_or_404(Poll, id=self.kwargs.get('pk'))
-        form.instance.poll = poll
-
+        answer = Answer()
+        for key, value in form.cleaned_data.items():
+            answer.poll_id=Poll.objects.get(id=self.kwargs.get('pk'))
+            answer.choice=value
+        answer.save()
         return redirect('index')
+
 
     def form_invalid(self, form):
         context = {'form': form}
         return render(self.request, self.template_name, context)
-
     def get_success_url(self):
         return reverse('index')
 
