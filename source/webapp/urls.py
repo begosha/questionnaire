7821bbd329b7fc4649e60293f3d51089ddd3699a
Gosha <begoshaaaa@gmail.com>
2021-04-03 13:31:18 +0600

    path to delete is added

--- source/webapp/urls.py
+++ source/webapp/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from webapp.views.poll_views import (IndexView, PollAddView, PollView, PollUpdateView)
+from webapp.views.poll_views import (IndexView, PollAddView, PollView, PollUpdateView, PollDeleteView)
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
     path('<int:pk>/', PollView.as_view(), name='poll'),
     path('add/', PollAddView.as_view(), name='poll-add'),
     path('<int:pk>/update/poll', PollUpdateView.as_view(), name='poll-update'),
+    path('<int:pk>/delete', PollDeleteView.as_view(), name='poll-delete'),
 ]
