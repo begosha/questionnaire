e537711dc21c666cde4becd4121921d7d0f2cc69
Gosha <begoshaaaa@gmail.com>
2021-04-03 18:41:29 +0600

    Answer form done

--- source/webapp/forms.py
+++ source/webapp/forms.py
@@ -1,8 +1,7 @@
 from django import forms
-from .models import Poll, Choice
+from .models import Poll, Choice, Answer
 
 class SimpleSearchForm(forms.Form):
-
     search = forms.CharField(max_length=100, required=False, label="Search")
 
 class PollForm(forms.ModelForm):
@@ -14,3 +13,14 @@ class ChoiceForm(forms.ModelForm):
     class Meta:
         model = Choice
         fields = ('text',)
+
+class AnswerForm(forms.ModelForm):
+    class Meta:
+        model = Answer
+        fields = ('choice',)
+
+    def __init__(self, poll, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['choice'].queryset = poll.master_poll.all()
+
+
