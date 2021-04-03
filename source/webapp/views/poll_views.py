efd845bf3d498a0cb0a861f874c64b0ea6caacef
Gosha <begoshaaaa@gmail.com>
2021-04-03 12:04:33 +0600

    orderinf in index view is applied

--- source/webapp/views/poll_views.py
+++ source/webapp/views/poll_views.py
@@ -12,6 +12,7 @@ class IndexView(ListView):
     template_name = 'poll/index.html'
     context_object_name = 'polls'
     model = Poll
+    ordering = ['-created_at']
     paginate_by = 5
     paginate_orphans = 1
 
