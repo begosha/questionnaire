f5756b1405797bff4620089c9e5827383ab041a5
Gosha <begoshaaaa@gmail.com>
2021-04-03 18:56:07 +0600

    saving is done

--- source/webapp/views/answer_views.py
+++ source/webapp/views/answer_views.py
@@ -21,7 +21,9 @@ class AnswerAddView(CreateView):
     def form_valid(self, form):
         poll = get_object_or_404(Poll, id=self.kwargs.get('pk'))
         form.instance.poll = poll
+
         return redirect('index')
+
     def form_invalid(self, form):
         context = {'form': form}
         return render(self.request, self.template_name, context)
